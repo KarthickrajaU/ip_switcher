954a698256dfcbc2f96422e701ff863eb387e233
Karthick Raja <74852121+KarthickrajaU@users.noreply.github.com>
2025-02-23 12:00:44 +0530

    code improved and comments added

--- ipswitcher.sh
+++ ipswitcher.sh
@@ -1,108 +1,127 @@
 #! /bin/bash
+
+# Automated IP Switcher
+# Developed by Karthick Raja
+# GitHub: https://github.com/karthickrajaU
+# Description: This script automatically switches the IP address using Tor at a specified interval.
+
 start=0
 
-echo -e "\e[32mAutomated IP switcher\e[0m"
+# Display banner information
+echo -e "\e[32mAutomated IP Switcher\e[0m"
 echo -e "\e[32mDeveloped by Karthick Raja\e[0m"
 echo -e "\e[32mhttps://github.com/karthickrajaU\e[0m"
 echo "---x---------x-----------x-------x------"
 
+# Define the website to check the current IP
 ip_site="https://icanhazip.com/"
 
-time_conv (){
- given_time=$(echo $1)
-if [ "$given_time" != "" ];then
- setted_time=$(($given_time*60))
-
-else
- given_time=30   #defalut time interval
- setted_time=18000 
-
-fi
-
+# Function to convert input time into seconds
+time_conv () {
+  given_time=$1
+  if [ -n "$given_time" ]; then
+    setted_time=$(($given_time * 60))
+  else
+    given_time=30  # Default time interval (30 minutes)
+    setted_time=18000
+  fi
 }
 
-close_term(){
-
-killid=$(ps aux|grep xterm|awk '{print $2}'|head -n1)
-kill $killid
+# Function to close any existing Tor terminal
+close_term() {
+  killid=$(ps aux | grep xterm | awk '{print $2}' | head -n1)
+  kill $killid
 }
 
-check_term(){
-term_status="[Status] open tor terminal "
-if [ $start != 0 ];then
-echo "[Status] closing existing terminal"
-close_term
-fi
-echo -e "\e[33m$term_status\e[0m"
-current_tab=$(tty)
-ls /dev/pts|sed 's/ /\n/g'>/var/tmp/old.tmp.1
-xterm &
-sleep 2
-ls /dev/pts|sed 's/ /\n/g'>/var/tmp/new.tmp.1
-open_tab=$(diff /var/tmp/old.tmp.1 /var/tmp/new.tmp.1|grep "^>"|tr -dc '[0-9]'|head -n1)
-exec tor  >/dev/pts/"$open_tab" &
-sleep 4
-return $open_tab
+# Function to check and start a new Tor terminal
+check_term() {
+  term_status="[Status] Opening Tor terminal"
+  if [ $start -ne 0 ]; then
+    echo "[Status] Closing existing terminal"
+    close_term
+  fi
+  echo -e "\e[33m$term_status\e[0m"
+  
+  # Capture current terminal tabs
+  ls /dev/pts | sed 's/ /\n/g' > /var/tmp/old.tmp.1
+  xterm &
+  sleep 2
+  ls /dev/pts | sed 's/ /\n/g' > /var/tmp/new.tmp.1
+  open_tab=$(diff /var/tmp/old.tmp.1 /var/tmp/new.tmp.1 | grep "^>" | tr -dc '[0-9]' | head -n1)
+  exec tor > /dev/pts/"$open_tab" &
+  sleep 4
+  return $open_tab
 }
 
+# Initialize Tor terminal
 check_term
 id=$(echo $?)
 
-tor_stat=$(echo $?)
-if [ '$tor_stat' == '0' ];then
-echo "Please run tor first then run this script"
-exit 1
+tor_stat=$?
+if [ "$tor_stat" -ne 0 ]; then
+  echo "Please run Tor first, then execute this script."
+  exit 1
 fi
 
 echo "Automatic IP Switcher"
 
-echo "Enter interval time :(when you want swap your IP): [default 30minutes]"
+# User input for interval time
+echo "Enter interval time (in minutes) for IP swap [Default: 30]:"
 read time
+
 time_conv $time
-echo "Enter max rounds you want to swap the IP"
+
+echo "Enter the maximum number of rounds for IP swapping [Default: 10]:"
 read max
 
-if [ "$max" == "" ];then
-max=10   #default 10 rounds
+if [ -z "$max" ]; then
+  max=10  # Default to 10 rounds
 fi
 
-echo "Setted time interval : $given_time minutes"
-echo "Setted max rounds to swap the IP : $max "
+echo "Set interval time: $given_time minutes"
+echo "Maximum IP swaps: $max"
 
-for i in $(seq 1 $max);do
-if [ $i == 1 ];then
-OP="hidden"
-#comment out below line if you want to know your REAL IP 
-#OP=`curl -s $ip_site`
-
-
-echo "Your original ip:$OP"
-fi
-
-IP=`curl -s --socks5-hostname 127.0.0.1:9050 $ip_site`
-
-echo -e "Your Spoofed IP:\e[31m $IP \e[0m"
-
-notify-send -t 5 $IP
-torify curl -s https://ip-api.io/json/$IP|sed 's/,/\n/g'|tr -d '{''"''}'|grep "^co\|^is\|^l\|^ip"
-echo -e "\e[32mUSE any kali tools with prefix TORIFY \e[0m"
-echo -e "\e[31mDon't close this tab it will close automatically when the timer finish\e[0m"
-sleep $setted_time
-
-echo "[Status]initializing Tor service restart operation"
-no=$(pidof tor)
-kill $no
-echo "[Status] Tor service restart successfully"
-
-start=1
-sleep 3
-if [ $i != $max ];then
-
-check_term
-id=$(echo $?)
-fi
-echo "----------------------------------"
+# Loop to continuously change the IP address
+for i in $(seq 1 $max); do
+  if [ $i -eq 1 ]; then
+    OP="hidden"  # Hide real IP (can be uncommented to display real IP)
+    # OP=`curl -s $ip_site`
+    echo "Your original IP: $OP"
+  fi
+  
+  # Get new spoofed IP
+  IP=$(curl -s --socks5-hostname 127.0.0.1:9050 $ip_site)
+  echo -e "Your Spoofed IP: \e[31m$IP\e[0m"
+  
+  # Display notification
+  notify-send -t 5000 "New IP: $IP"
+  
+  # Get IP location details
+  torify curl -s https://ip-api.io/json/$IP | sed 's/,/\n/g' | tr -d '{"}' | grep "^co\|^is\|^l\|^ip"
+  
+  echo -e "\e[32mUse any Kali tools with the prefix 'TORIFY'\e[0m"
+  echo -e "\e[31mDo not close this tab; it will close automatically after the timer finishes.\e[0m"
+  
+  sleep $setted_time
+  
+  echo "[Status] Restarting Tor service"
+  no=$(pidof tor)
+  kill $no
+  echo "[Status] Tor service restarted successfully"
+  
+  start=1
+  sleep 3
+  
+  if [ $i -ne $max ]; then
+    check_term
+    id=$(echo $?)
+  fi
+  
+  echo "----------------------------------"
 done
+
+# Cleanup and exit
 close_term
 rm /var/tmp/*.tmp.1
-echo -e "\e[31m[Status]IP switcher closed \e[1m"
+
+echo -e "\e[31m[Status] IP switcher closed\e[0m"
